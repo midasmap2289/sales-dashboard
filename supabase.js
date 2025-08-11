14f86f03d833819073b8d67ac56d4089012f8d0f
midasmap2289 <mirski.midas@gmail.com>
2025-08-11 13:10:02 -0700

    Remove root-level supabase.js duplicate

--- supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-// Create a single supabase client for the project.  Reuse this across the app.
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
